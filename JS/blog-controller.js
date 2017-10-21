fd0a752f73be502cab2da57475230a0691278abf
Deanna Vickers <deanna2000@gmail.com>
2017-10-21 13:07:58 -0500

    lots of styling on nav and blog

--- JS/blog-controller.js
+++ JS/blog-controller.js
@@ -2,6 +2,6 @@ let blogInfo = document.getElementById("blogs")
 
 for (let i = 0; i < blogDatabase.length; i++) {
     let currentBlog = blogDatabase[i]
-   blogInfo.innerHTML += `<li class="${currentBlog.title}"><a href="${currentBlog.date}">${currentBlog.content}</a></li>`
+   blogInfo.innerHTML += `<section class="blogging"><p>${currentBlog.date}</p><h2> ${currentBlog.title}</h2><p>${currentBlog.content}</p></section>`
     console.log(currentBlog.title) 
 }
