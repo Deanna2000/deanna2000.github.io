ca51c5271ca6caa1a081b2e2c124369bfbfc7ede
Deanna Vickers <deanna2000@gmail.com>
2017-10-25 15:47:36 -0500

    added resume data and got the innerhtml working

--- Scripts/blog-controller.js
+++ Scripts/blog-controller.js
@@ -2,6 +2,13 @@ let blogInfo = document.getElementById("blogs")
 
 for (let i = 0; i < blogDatabase.length; i++) {
     let currentBlog = blogDatabase[i]
-   blogInfo.innerHTML += `<section class="blogging"><p>${currentBlog.date}</p><h2> ${currentBlog.title}</h2><p>${currentBlog.content}</p></section>`
+   blogInfo.innerHTML += 
+    `
+    <section class="blogging">
+        <p>${currentBlog.date}</p>
+        <h2> ${currentBlog.title}</h2>
+        <p>${currentBlog.content}</p>
+    </section>
+    `
     console.log(currentBlog.title) 
 }
--- Scripts/resume-factory.js
+++ Scripts/resume-factory.js
@@ -4,21 +4,21 @@ const resumeDatabase = [
         "company": "Doo Drop",
         "Location": "Nashville - Remote",
         "dates": "June 2016 - Present",
-        "accomplishments": ["created vision for mobile app called Doo Drop", "worked with team of designers to develop a website for Doo Drop", "paired with designer to create mockups for mobile app"]
+       accomplishments: ["Created vision for mobile app called Doo Drop", "Worked with team of designers to develop a website for Doo Drop", "Paired with designer to create mockups for mobile app"]
     },
     {
         "jobtitle": "Senior Engagement Manager",
         "company": "Acklen Avenue",
         "Location": "Nashville - Remote",
         "dates": "November 2014 - September 2017",
-        "accomplishments": ["Implemented new process for management of client deliveries", "worked with leadership to add Design to our development process", "Initiated and managed monthly company wide meetings"]
+        accomplishments: ["Implemented new process for management of client deliveries", "worked with leadership to add Design to our development process", "Initiated and managed monthly company wide meetings"]
     },
     {
         "jobtitle": "Director of Client Engagement", 
         "company": "appendTo",
         "Location": "Nashville - Remote",
         "dates": "May 2013 - November 2014",
-        "accomplishments": ["Implemented core scrum practices across all projects","Provided coaching and mentoring to Engagement Management team","Maintained client satisfaction by resolving concerns immediately"]
+        accomplishments: ["Implemented core scrum practices across all projects","Provided coaching and mentoring to Engagement Management team","Maintained client satisfaction by resolving concerns immediately"]
     },
     {
         "jobtitle": "Consultant", 
@@ -28,3 +28,5 @@ const resumeDatabase = [
         "accomplishments": ["Led a large project to help manage home energy assessment information for the state of TN","Managed several large telecom insuarance projects ","Created plan for a clinical research study application for a orthopaedic surgery practice"]
     }
     ]
+
+    
