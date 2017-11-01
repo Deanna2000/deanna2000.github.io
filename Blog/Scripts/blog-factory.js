a171d8e9b716cbb6a74e4c703d3dcd7614217477
Deanna Vickers <deanna2000@gmail.com>
2017-11-01 13:06:49 -0500

    switching to work on the search feature

--- Blog/Scripts/blog-factory.js
+++ Blog/Scripts/blog-factory.js
@@ -13,5 +13,46 @@ const blogDatabase = [
     "title": "Still feeling positive", 
     "date": "Week 3 - October 20",
     "content": "Things are beginning to get very complex and I'm understanding about 30% of it. But I'm not going to let it get me down, I'll just keep plugging away at my own pace and focus on putting the pieces together. I'm already feeling pretty good about using git and github so that's a huge help. Last week and this week we split up into small groups to work on projects. I'm finding everyone in the class to be really fun to work with - I guess that's a product of people who are invested in learning together."
+},
+{
+    "title": "It's really real now", 
+    "date": "Week 4 - October 27",
+    "content": "I'm still lagging behind in my exercises but I am pretty happy with the progress I'm making in learning about web development. I know so much more now than I did when I got here - I can't believe how quickly it's coming. I'm getting comfortable using git and github and of course keeping up with tickets on our project boards is something I'm really tuned into. This week I finished up some tickets for my personal site and switched back to doing work on exercises. Next up is Colored Reindeer and I admit, I am clueless about this exercise. "
+},
+{
+    "title": "Coming up out of confusion", 
+    "date": "Week 5 - November 3",
+    "content": "Wow, I finished Colored Reindeer and I'm on my way to start adding pagination and searching to my blog. One thing that has occurred to me is not just a new way of thinking but really understanding the steps that are involved in writing code as it relates to the steps of delivering features. I knew they were not necessarily directly related but it's taking on a whole new meaning now. Knowing what to do is very different than knowing how to do it. And therein lies the 'language' aspect of what we are learning. For example, when I say something in English and want to translate it to French, I need to remember that the words are not the only thing that changes, there is also a new order to the sentence and in some cases, it's expressed in a way that does not translate directly which requires even more creativity to develop an approximation of what was intended."
+},
+{
+    "title": "Testing Page 2", 
+    "date": "Week 6 - November 3",
+    "content": "Wow, I finished Colored Reindeer and I'm on my way to start adding pagination and searching to my blog. One thing that has occurred to me is not just a new way of thinking but really understanding the steps that are involved in writing code as it relates to the steps of delivering features. I knew they were not necessarily directly related but it's taking on a whole new meaning now. Knowing what to do is very different than knowing how to do it. And therein lies the 'language' aspect of what we are learning. For example, when I say something in English and want to translate it to French, I need to remember that the words are not the only thing that changes, there is also a new order to the sentence and in some cases, it's expressed in a way that does not translate directly which requires even more creativity to develop an approximation of what was intended."
+},
+{
+    "title": "Testing Page 2", 
+    "date": "Week 7 - November 3",
+    "content": "Wow, I finished Colored Reindeer and I'm on my way to start adding pagination and searching to my blog. One thing that has occurred to me is not just a new way of thinking but really understanding the steps that are involved in writing code as it relates to the steps of delivering features. I knew they were not necessarily directly related but it's taking on a whole new meaning now. Knowing what to do is very different than knowing how to do it. And therein lies the 'language' aspect of what we are learning. For example, when I say something in English and want to translate it to French, I need to remember that the words are not the only thing that changes, there is also a new order to the sentence and in some cases, it's expressed in a way that does not translate directly which requires even more creativity to develop an approximation of what was intended."
+},
+{
+    "title": "Testing Page 2", 
+    "date": "Week 8 - November 3",
+    "content": "Wow, I finished Colored Reindeer and I'm on my way to start adding pagination and searching to my blog. One thing that has occurred to me is not just a new way of thinking but really understanding the steps that are involved in writing code as it relates to the steps of delivering features. I knew they were not necessarily directly related but it's taking on a whole new meaning now. Knowing what to do is very different than knowing how to do it. And therein lies the 'language' aspect of what we are learning. For example, when I say something in English and want to translate it to French, I need to remember that the words are not the only thing that changes, there is also a new order to the sentence and in some cases, it's expressed in a way that does not translate directly which requires even more creativity to develop an approximation of what was intended."
+},
+{
+    "title": "Testing Page 2", 
+    "date": "Week 9 - November 3",
+    "content": "Wow, I finished Colored Reindeer and I'm on my way to start adding pagination and searching to my blog. One thing that has occurred to me is not just a new way of thinking but really understanding the steps that are involved in writing code as it relates to the steps of delivering features. I knew they were not necessarily directly related but it's taking on a whole new meaning now. Knowing what to do is very different than knowing how to do it. And therein lies the 'language' aspect of what we are learning. For example, when I say something in English and want to translate it to French, I need to remember that the words are not the only thing that changes, there is also a new order to the sentence and in some cases, it's expressed in a way that does not translate directly which requires even more creativity to develop an approximation of what was intended."
+},
+{
+    "title": "Testing Page 2", 
+    "date": "Week 10 - November 3",
+    "content": "Wow, I finished Colored Reindeer and I'm on my way to start adding pagination and searching to my blog. One thing that has occurred to me is not just a new way of thinking but really understanding the steps that are involved in writing code as it relates to the steps of delivering features. I knew they were not necessarily directly related but it's taking on a whole new meaning now. Knowing what to do is very different than knowing how to do it. And therein lies the 'language' aspect of what we are learning. For example, when I say something in English and want to translate it to French, I need to remember that the words are not the only thing that changes, there is also a new order to the sentence and in some cases, it's expressed in a way that does not translate directly which requires even more creativity to develop an approximation of what was intended."
+},
+{
+    "title": "Testing Page 2", 
+    "date": "Week 5 - November 3",
+    "content": "Wow, I finished Colored Reindeer and I'm on my way to start adding pagination and searching to my blog. One thing that has occurred to me is not just a new way of thinking but really understanding the steps that are involved in writing code as it relates to the steps of delivering features. I knew they were not necessarily directly related but it's taking on a whole new meaning now. Knowing what to do is very different than knowing how to do it. And therein lies the 'language' aspect of what we are learning. For example, when I say something in English and want to translate it to French, I need to remember that the words are not the only thing that changes, there is also a new order to the sentence and in some cases, it's expressed in a way that does not translate directly which requires even more creativity to develop an approximation of what was intended."
 }
+
 ]
