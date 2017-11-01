6276cd3856f7e799bf9fab6668e2ef316b0756e4
Deanna Vickers <deanna2000@gmail.com>
2017-11-01 15:44:33 -0500

    added comments for the form objectives

--- Blog/Scripts/blog-sort.js
+++ Blog/Scripts/blog-sort.js
@@ -1,23 +1,23 @@
 
 // Get the database from local storage
-const bloglDatabase = JSON.parse(
-    localStorage.getItem("blogDatabase")
-) || {}
+// const bloglDatabase = JSON.parse(
+//     localStorage.getItem("blogDatabase")
+// ) || {}
 
 // Create `blogPosts` key - WHAT IS THIS FOR?
-bloglDatabase.blogPosts = bloglDatabase.blogPosts || []
+// bloglDatabase.blogPosts = bloglDatabase.blogPosts || []
 
 
 // generate ID's for the blog entries
-const idGen = function* (from) {
-    let id = 1
-    while (true) {
-        yield from + id
-        id++
-        console.log(id)
-    }
-}
-const postIDGen = idGen();
+// const idGen = function* (from) {
+//     let id = 1
+//     while (true) {
+//         yield from + id
+//         id++
+//         console.log(id)
+//     }
+// }
+// const postIDGen = idGen();
 
 
 
@@ -42,5 +42,4 @@ const postIDGen = idGen();
 // const blogPostFactory = postFactory()
 
 // Sort the articles by their `id` property, descending
-bloglDatabase.blogPosts.sort((p, n) => n.id - p.id)
-console.log(id)
+// bloglDatabase.blogPosts.sort((p, n) => n.id - p.id)
---
+++ admin/Scripts/blog-form.js
@@ -0,0 +1,10 @@
+//create form elements
+//id, date, title, body
+
+
+//event listener for button click to submit form
+
+//store blog post somewhere
+
+//display blog posts
+
